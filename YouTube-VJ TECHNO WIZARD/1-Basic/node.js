071e3e325be6b9807c1d0fb9fc4d1ce1b3da0390
Just-Tamizha <protamizhahelp@gmail.com>
2022-03-23 09:30:15 +0530

    YT-VJ-Basic-Add Files and work

---
+++ YouTube-VJ TECHNO WIZARD/1-Basic/node.js
@@ -0,0 +1,4 @@
+// Step-1: write some code for run node js
+console.log("Tamizha")
+
+// Step-2: Run teriminal (ctrl+shift+`) type cmd "node (filename)" Example : "node node.js" or "node node"(No need add file type)
